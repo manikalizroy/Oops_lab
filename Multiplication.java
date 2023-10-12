4c8b56971c06630246917baa3b2d3fc361eda3c8
manikalizroy <118897269+manikalizroy@users.noreply.github.com>
2023-10-12 22:40:00 +0530

    Update Multiplication.java

--- Multiplication.java
+++ Multiplication.java
@@ -1,67 +1,70 @@
-class Matmulti
+class Multipication
 {
-    int m,n,p,q;
-    int[][] c = new int[m][q];
-    int i,j,k;
+    private int m,n,p,q;
+    private int[][] c;
     
-    public Matmulti(int m, int n, int p, int q) {
+    public Multipication(int m, int n, int p, int q)
+    {
         this.m = m;
         this.n = n;
         this.p = p;
         this.q = q;
-        c = new int[m][q]; // Result matrix dimensions (m x q)
+        c = new int[20][20];
     }
     
-    void displaymat(int[][] x,int y,int z)
+    public void display(int[][] x,int m,int n)
     {
-    	
-        for(i=0;i<y;i++)
+        int i,j,k;
+        for(i=0;i<m;i++)
         {
-            for(j=0;j<z;j++)
+            for(j=0;j<n;j++)
             {
-                 System.out.print(x[i][j] + " "); 
+                System.out.print(x[i][j] + " ");
             }
             System.out.println();
-        }    
-    }
+        }
+    } 
     
-    void multiply(int [][] a,int[][] b)
+    public void multiply(int[][] a,int[][] b)
     {
+        int i,j,k;
         for(i=0;i<m;i++)
             for(j=0;j<q;j++)
+            {
+                c[i][j] = 0;
                 for(k=0;k<n;k++)
                     c[i][j] += a[i][k]*b[k][j];
+            }     
     }
     
-    void printprod()
+    public void print()
     {
-        System.out.println("Product:");
-        for (i = 0; i < m; i++) 
+        int i,j;
+        for(i=0;i<m;i++)
         {
-            for (j = 0; j < q; j++) 
+            for(j=0;j<q;j++)
             {
                 System.out.print(c[i][j] + " ");
             }
             System.out.println();
         }    
     }
-        
 }
 
 public class Main
 {
     public static void main(String[] args)
     {
-        int m=3,n = 2,p=2,q=3;
-        Matmulti m1 = new Matmulti(m,n,p,q);
-        int[][] a = {{1,2},{2,3},{1,1}};
-        int[][] b = {{1,3,0},{2,1,3}};
-        System.out.println("Matrix A:");
-        m1.displaymat(a,m,n);
-        System.out.println("Matrix B:");
-        m1.displaymat(b,n,q);
+        int m=3,n=2,p=2,q=3;
+        Multipication m1 = new Multipication(m,n,p,q);
+        int[][] a = {{1,3},{4,1},{1,1}};
+        int[][] b = {{3,2,0},{0,1,2}};
+        System.out.println("Matrix A: ");
+        m1.display(a,m,n);
+        System.out.println("Matrix B: ");
+        m1.display(b,p,q);
         m1.multiply(a,b);
-        m1.printprod();
-      
+        System.out.println("Product Matrix : ");
+        m1.print();
     }
 }
