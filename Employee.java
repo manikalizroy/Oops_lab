e15e9c961650b0cf045715a11cae049bb3812328
manikalizroy <118897269+manikalizroy@users.noreply.github.com>
2023-10-13 19:49:15 +0530

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,59 @@
+
+class Employee
+{
+    String name;
+    int age,phno,salary;
+    String address;
+  
+    public Employee(String name,int age,int phno,int salary,String address)
+    {
+        this.name = name;
+        this.age = age;
+        this.phno = phno;
+        this.address = address;
+        this.salary = salary;
+    }
+ 
+    public void printsalary()
+    {
+        System.out.println("Manager's Salary is: " + salary);
+    }
+}
+
+class Officer extends Employee
+{
+    String specialization;
+    public Officer(String name,int age,int phno,int salary,String address)
+    {
+        super(name,age,phno,salary,address);
+        this.specialization = specialization;
+    }
+    public void printsalary()
+    {
+        System.out.println("Officer's Salary is: " + salary);
+    }
+    
+    
+}
+
+class Manager extends Employee
+{
+    String department;
+    public Manager(String name,int age,int phno,int salary,String address)
+    {
+        super(name,age,phno,salary,address);
+        this.department = department;
+    }
+}
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Officer O1 = new Officer("Liza",40,77567,200000,"Home");
+        Manager M1 = new Manager("Aza",35,646366,400000,"Work");
+        
+        O1.printsalary();
+        M1.printsalary();
+    }
+}
