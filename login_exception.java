bc58737c9bcb976d53f97bb868b8e573fb01ac28
manikalizroy <118897269+manikalizroy@users.noreply.github.com>
2024-02-07 17:25:24 +0000

    Create login_exception.java

---
+++ login_exception.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+class PasswordException extends Exception
+{
+    protected String msg;
+    public PasswordException(String msg)
+    {
+        this.msg = msg;
+    }
+    public String getMsg()
+    {
+        return msg;
+    }
+}
+
+public class Main
+{
+	public static void main(String[] args) 
+	{
+	    try 
+	    {
+    	    Scanner sc = new Scanner(System.in);
+    		System.out.println("Enter username");
+    		String user = sc.nextLine();
+    		System.out.println("Enter password");
+    		String password = sc.nextLine();
+    		if(user.equals("admin") && password.equals("password"))
+    		    System.out.println("Login Successfull");
+    		else
+    		{
+    		    throw new PasswordException("Invalid Login Credentials");
+    		}
+	    }
+	    catch(PasswordException e)
+	    {
+	        System.out.println(e.getMsg());
+	    }
+	        
+	    }
+}
